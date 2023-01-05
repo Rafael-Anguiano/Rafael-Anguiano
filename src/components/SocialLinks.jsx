bd18d0f0c8a295d0e775f8d3256bfef516b69059
Rafael Anguiano <rj.anguiano.23@gmail.com>
2023-01-05 13:19:01 -0600

    Resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size={30}/>
                 </>
             ),
-            href: '../Resume.pdf',
+            href: '../Resume-Rafael-Anguiano.pdf',
             style: 'rounded-br-md',
             download: true,
         },
